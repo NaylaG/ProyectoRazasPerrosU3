eeef378121fb02ea2d526e0cee17c95e18d6bbce
Nayla Alejandra Garcia Mireles <171G0227@rcarbonifera.tecnm.mx>
2020-11-02 15:59:23 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoRazasPerrosU3/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoRazasPerrosU3.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult Eliminar()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProyectoRazasPerrosU3/Models/ViewModels/RazaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoRazasPerrosU3.Models.ViewModels
+{
+    public class RazaViewModel
+    {
+        public uint Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
