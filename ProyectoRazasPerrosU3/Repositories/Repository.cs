0b2a100357c2f81ea7797fcecdc288e776fa3dd1
Nayla Alejandra Garcia Mireles <171G0227@rcarbonifera.tecnm.mx>
2020-11-05 11:36:52 -0600

    Nuevo repositorio agregado

---
+++ ProyectoRazasPerrosU3/Repositories/Repository.cs
@@ -0,0 +1,64 @@
+﻿using ProyectoRazasPerrosU3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoRazasPerrosU3.Repositories
+{
+    public class Repository<T> where T: class
+    {
+        public sistem14_razasContext Context { get; set; }
+
+        public Repository(sistem14_razasContext context)
+        {
+            Context = context;
+        }
+		public virtual IEnumerable<T> GetAll()
+		{
+			return Context.Set<T>();
+		}
+
+		public virtual T GetById(object id)
+		{
+			return Context.Find<T>(id);
+		}
+
+		public virtual void Insert(T entidad)
+		{
+			if (Validate(entidad))
+			{
+				Context.Add(entidad);
+				Save();
+			}
+		}
+
+		public virtual void Update(T entidad)
+		{
+			if (Validate(entidad))
+			{
+				Context.Update(entidad);
+				Save();
+			}
+		}
+
+		public virtual void Delete(T entidad)
+		{
+
+			Context.Remove(entidad);
+			Save();
+
+		}
+
+		public virtual void Save()
+		{
+			Context.SaveChanges();
+		}
+
+		public virtual bool Validate(T entidad)
+		{
+			return true;
+		}
+
+	}
+}
